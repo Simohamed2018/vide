0290ae5d408ba85290ee37cafb5f17f7e9362257
Simohamed <Simohamed@simoAdam>
2018-05-03 00:12:26 -0400

    add hach code and equal

--- demo/src/main/java/com/example/demo/entitie/Voiture.java
+++ demo/src/main/java/com/example/demo/entitie/Voiture.java
@@ -33,6 +33,38 @@ public class Voiture {
 	public String toString() {
 		return "Voiture [id=" + id + ", marque=" + marque + ", modele=" + modele + "]";
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((marque == null) ? 0 : marque.hashCode());
+		result = prime * result + ((modele == null) ? 0 : modele.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Voiture other = (Voiture) obj;
+		if (id != other.id)
+			return false;
+		if (marque == null) {
+			if (other.marque != null)
+				return false;
+		} else if (!marque.equals(other.marque))
+			return false;
+		if (modele == null) {
+			if (other.modele != null)
+				return false;
+		} else if (!modele.equals(other.modele))
+			return false;
+		return true;
+	}
 	
 
 }
