b385c63c203db6ab0ce3c77937cf834d4109fc16
Simohamed <Simohamed@simoAdam>
2018-05-02 23:41:54 -0400

    first commit

---
+++ demo/src/main/java/com/example/demo/entitie/Voiture.java
@@ -0,0 +1,27 @@
+package com.example.demo.entitie;
+
+public class Voiture {
+	private long id;
+	private String marque;
+	private String modele;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMarque() {
+		return marque;
+	}
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+	public String getModele() {
+		return modele;
+	}
+	public void setModele(String modele) {
+		this.modele = modele;
+	}
+	
+
+}
